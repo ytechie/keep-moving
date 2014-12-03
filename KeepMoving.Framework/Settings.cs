88afb6592a44e1694785cd6d2a185e7225d0ab88
Carl Schweitzer <carl@caschw.com>
2014-12-02 22:25:48 -0600

    Refactored settings
    Refactored settings to put common logic in two helper functions. This
    simplifies set up time of any new settings that need to be added.

--- KeepMoving.Framework/Settings.cs
+++ KeepMoving.Framework/Settings.cs
@@ -1,33 +1,57 @@
 ﻿using Windows.Storage;
+using System.Collections.Generic;
 
 namespace KeepMoving.Framework
 {
     public class Settings
     {
         private const string DeviceSettingsContainerName = "DeviceSettings";
+        private const string TimeoutPeriodSettingName = "TimeoutPeriod";
         private const string TrackingEnabledSettingName = "TrackingEnabled";
 
         public static void SetTrackingEnabled(bool enabled)
         {
-            var settingsContainer = ApplicationData.Current.LocalSettings.CreateContainer(DeviceSettingsContainerName,
-    ApplicationDataCreateDisposition.Always);
-
-            settingsContainer.Values[TrackingEnabledSettingName] = enabled;
+            SetSetting(TrackingEnabledSettingName, enabled);
         }
 
         public static bool GetTrackingEnabled()
         {
+            return GetSetting<bool>(TrackingEnabledSettingName);
+        }
+
+        public static void SetTimeoutPeriod(int newValue)
+        {
+            SetSetting(TimeoutPeriodSettingName, newValue);
+        }
+
+        public static int GetTimeoutPeriod()
+        {
+            var value = GetSetting<int>(TimeoutPeriodSettingName);
+            return value == default(int) ? 15 : value;
+        }
+
+        private static T GetSetting<T>(string settingName)
+        {
             var settingsContainer = ApplicationData.Current.LocalSettings.CreateContainer(DeviceSettingsContainerName,
                 ApplicationDataCreateDisposition.Always);
 
-            var settingObj = settingsContainer.Values[TrackingEnabledSettingName];
+            var settingObj = settingsContainer.Values[settingName];
 
             if (settingObj == null)
             {
-                return true; //default
+                return default(T); //default
             }
 
-            return (bool) settingObj;
+            return (T)settingObj;
+        }
+
+        private static void SetSetting(string settingName, object value)
+        {
+            var settingsContainer = ApplicationData.Current.LocalSettings.CreateContainer(DeviceSettingsContainerName,
+    ApplicationDataCreateDisposition.Always);
+
+            settingsContainer.Values[settingName] = value;
+
         }
     }
 }
